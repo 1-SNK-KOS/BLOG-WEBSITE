309ad9f9a59703cdf594a54d6d056aa42f5b462f
Gautam Chaurasiya <150584596+1-SNK-KOS@users.noreply.github.com>
2024-10-18 00:42:55 +0530

    first commit

---
+++ DEV_Blog/src/config/config.js
@@ -0,0 +1,10 @@
+
+const credentials = {
+    appwrite_endpoint: String(import.meta.env.VITE_APPWRITE_URL), //url
+    appwrite_database: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwrite_project: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwrite_collection: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwrite_storage: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
+
+export default credentials;
